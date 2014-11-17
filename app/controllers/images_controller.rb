b72623c687bc1f22c1fe02f3ce28dd843b1effc0
eduardtarassov <eduardtarassov@gmail.com>
2014-11-17 14:46:17 +0000

    Css.scss works perfectly now. Database find and create methods work well!

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -40,4 +40,14 @@ class ImagesController < ApplicationController
   def image_params
     params.require(:image).permit(:title, :description, :category, :image_url, :voice_url)
   end
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @image.destroy
+    respond_to do |format|
+      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 end
