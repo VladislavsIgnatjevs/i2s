7b38b37c5cc35cdcbf80ea34b7c03401b9644ced
eduardtarassov <eduardtarassov@gmail.com>
2014-11-23 21:07:57 +0000

    Creation and editing of images work better.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -31,6 +31,20 @@ class ImagesController < ApplicationController
   def edit
   end
 
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    respond_to do |format|
+      if @image.update(image_params)
+        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.json { render :show, status: :ok, location: @image }
+      else
+        format.html { render :edit }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_image
